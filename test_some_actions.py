67826f205a4951267467d5f45e1f5d3eb0a525dc
Goncharov Anton <gonchrrv@icloud.com>
2024-05-09 14:43:22 +0300

    upd google search

--- test_some_actions.py
+++ test_some_actions.py
@@ -11,29 +11,29 @@ password = os.getenv("OWN_PASS")
 google = os.getenv('GOOGLE')
 
 
-def test_account_login():
-    browser.open(qa_guru)
-    s('[name=email]').type(login)
-    s('[name=password]').type(password).press_enter()
-    s('.xdget-block').should(have.text('Актуальные события'))
-    s('.page-header').should(have.text('Список тренингов'))
-
-    s('[title=Профиль]').click()
-    s('.subitem-link').should(be.clickable)
-
-    ss('.subitem-link')[3].should(have.text('ВЫЙТИ')).click()
-    s('#xdget172046_1').should(have.text('Войти'))
-
-    s('[name=email]').should(be.blank).click().type('fakemail')
-    s('[name=password]').should(be.blank).click().type(password)
-    s('#xdget33092_1').should(have.text('Войти')).click()
-    s('.btn-error').should(have.text('Неверный формат e-mail'))
+# def test_account_login():
+#     browser.open(qa_guru)
+#     s('[name=email]').type(login)
+#     s('[name=password]').type(password).press_enter()
+#     s('.xdget-block').should(have.text('Актуальные события'))
+#     s('.page-header').should(have.text('Список тренингов'))
+#
+#     s('[title=Профиль]').click()
+#     s('.subitem-link').should(be.clickable)
+#
+#     ss('.subitem-link')[3].should(have.text('ВЫЙТИ')).click()
+#     s('#xdget172046_1').should(have.text('Войти'))
+#
+#     s('[name=email]').should(be.blank).click().type('fakemail')
+#     s('[name=password]').should(be.blank).click().type(password)
+#     s('#xdget33092_1').should(have.text('Войти')).click()
+#     s('.btn-error').should(have.text('Неверный формат e-mail'))
 
 
 def test_google_search():
     browser.open(google)
     s('[name=q]').type('yashaka/selene').press_enter()
-    ss('.LC20lb')[0].should(have.text('yashaka/selene: User-oriented Web UI browser tests in')).click()
+    ss('#search h3').element_by(have.text('yashaka/selene: User-oriented Web UI')).click()
     s('.my-3').should(have.text('User-oriented Web UI browser tests in Python'))
 
 
